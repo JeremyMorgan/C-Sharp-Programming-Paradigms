72a014583bc885dfd51abce74fafda0c4912f292
Jeremy Morgan <jeremymorgan@gmail.com>
2015-04-02 14:32:35 -0700

    Broken demo

--- C Programming Paradigms/Program.cs
+++ C Programming Paradigms/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace C_Programming_Paradigms
 {
@@ -12,33 +11,36 @@ namespace C_Programming_Paradigms
         static void Main(string[] args)
         {
             IEnumerable<string> cities = new[] { "Cornelius", "Hillsboro", "Forest Grove", "Gaston" };
-            IEnumerable<string> query = cities.StringsThatStartWith("F");
-
-            
+            IEnumerable<string> query = cities.Filter(StringsThatStartWithL);            
             
             foreach (var city in query){
 
                 Console.WriteLine(city);
 
             }
+        }
 
-            Console.ReadLine();
+        static bool StringsThatStartWithL(string s)
+        {
+            return s.StartsWithL("L");
         }
     }
 
     public static class FilterExtensions
     {
-        public static IEnumerable<string> StringsThatStartWith(this IEnumerable<string> input, string start)
+        public static IEnumerable<string> Filter(this IEnumerable<string> input, FilterDelegate<string> predicate)
         {
-            foreach (var s in input)
+            foreach (var item in input)
             {
-                if (s.StartsWith(start))
+                if (predicate(item))
                 {
-                    yield return s;
+                    yield return item;
                 }
             }
 
         }
+
+        public delegate bool FilterDelegate<T>(T item);
     }
    
 }
